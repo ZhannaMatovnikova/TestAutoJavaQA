09c61643c9563dd60dbe3c10bcba53ec0661dc29
Жанна <jane95@outlook.com>
2023-12-20 17:58:40 +0300

    MainPageLocators

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,19 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+    static WebDriver driver;
+    public static void main(String[] args) {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://mail.ru/");
+
+        MainPage mainPage = new MainPage(driver);
+
+        driver.quit();
+    }
+}
---
+++ src/main/java/MainPage.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+    private WebDriver driver;
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By loginMainButton = By.xpath(""); //
+    private By accountNameField = By.xpath("");
+    private By addPasswordButton = By.xpath("");
+    private By PasswordField = By.xpath("");
+    private By loginButtonForm = By.xpath("");
+    private By iconAvatar = By.xpath("");
+    private By linkEmailAvatar = By.xpath("");
+    private By newMessageButton = By.xpath("");
+    private By adddressField = By.xpath("");
+    private By themeField = By.xpath("");
+    private By messageBodyField = By.xpath("");
+    private By saveMessageButton = By.xpath("");
+
+
+
+}
